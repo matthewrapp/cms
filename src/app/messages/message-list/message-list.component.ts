bc838bce3de7f740240030c6ffe9b5922c1c6e9d
Matthew Rapp <mattrapp25@gmail.com>
2021-10-02 16:04:01 -0600

    receive and display messages done

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message('1', 'Test subject #1', 'Test message number 1... doing this to make it longer.', 'Matthew'),
+    new Message('2', 'Test subject #2', 'Test message number 2... doing this to make it longer, even longer!', 'Matthew'),
+    new Message('3', 'Test subject #3', 'Test message number 3... doing this to make it longer, even longer longer!!', 'Matthew'),
+    new Message('4', 'Test subject #4', 'Test message number 4... doing this to make it longer, even longer longer longer!!!', 'Matthew'),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
   }
 
 }
