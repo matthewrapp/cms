481a2a23b7b674c29ac8010a23b1d2eb5458feb8
Matthew Rapp <mattrapp25@gmail.com>
2021-10-22 17:53:27 -0600

    done

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -10,10 +12,18 @@ export class ContactDetailComponent implements OnInit {
   contacts: Contact[] = [];
   @Input() contact: Contact;
 
-  constructor() {  }
+  constructor(private contactService: ContactService, private route: ActivatedRoute, private router: Router) {  }
 
   ngOnInit(): void {
-    
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      this.contact = this.contactService.getContact(id);
+    })
+  }
+
+  onDelete() {
+    this.contactService.deleteContact(this.contact);
+    this.router.navigateByUrl('/contacts')
   }
 
 }
--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -6,6 +6,7 @@ import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 export class DocumentService {
   documents: Document[] = [];
   documentSelectEvent = new EventEmitter<Document>();
+  documentChangedEvent = new EventEmitter<Document[]>();
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
@@ -17,5 +18,13 @@ export class DocumentService {
 
   getDocument(id: string) {
     return this.documents.find(document => document.id === id ? document : null);
+  }
+
+  deleteDocument(document: Document) {
+    if (!document) return;
+    const pos = this.documents.indexOf(document);
+    if (pos < 0) return;
+    this.documents.splice(pos, 1);
+    this.documentChangedEvent.emit(this.documents.slice());
   }
 }
