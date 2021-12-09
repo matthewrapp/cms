a4da7d53006b0425fa1542ba1077d16e59e91949
matthewrapp <mattrapp25@gmail.com>
2021-12-09 09:52:12 -0700

    final changes

--- server/models/sequence.js
+++ server/models/sequence.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 const sequenceSchema = mongoose.Schema({
     maxDocumentId: { type: Number, require: true },
     maxMessageId: { type: Number, require: true },
-    MaxContactId: { type: Number, require: true },
+    maxContactId: { type: Number, require: true },
 });
 
 module.exports = mongoose.model('Sequence', sequenceSchema);
